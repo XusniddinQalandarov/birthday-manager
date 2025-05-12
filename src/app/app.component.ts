d4e0bc37f58fd42118138c16a5139d0c64483f0a
XusniddinQalandarov <x.qalandarov@newuzbekistanuniversity.uz>
2025-05-12 14:52:41 +0500

    vercel ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BirthdayFormComponent } from './birthday-form/birthday-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [BirthdayFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'birthday-manager';
